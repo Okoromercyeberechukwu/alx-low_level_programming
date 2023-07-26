217564462d9ff09595ae287af7ee2f30ef05f5bb
Okoromercyeberechukwu <okoromercyebere@gmail.com>
2023-07-26 15:08:34 -0700

    Done here

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,27 +1,29 @@
 #include "main.h"
-
 /**
- *  * rot13 - encodes a string in rot13
- *   * @s: string to be encoded
- *    *
- *     * Return: the resulting string
- *      */
-char *rot13(char *s)
-{
-		int i, j;
-			char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-				char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+ * leet - encodes a string into 1337
+ * @s: input string.
+ * Return: the pointer to dest.
+ */
 
-					for (i = 0; s[i] != '\0'; i++)
-							{
-										for (j = 0; a[j] != '\0'; j++)
-													{
-																	if (s[i] == a[j])
-																					{
-																										s[i] = b[j];
-																														break;
-																																	}
-																			}
-											}
-						return (s);
+char *leet(char *s)
+{
+	int count = 0, i;
+	int low_letters[] = {97, 101, 111, 116, 108};
+	int upp_letters[] = {65, 69, 79, 84, 76};
+	int numbers[] = {52, 51, 48, 55, 49};
+
+	while (*(s + count) != '\0')
+	{
+		for (i = 0; i < 5; i++)
+		{
+			if (*(s + count) == low_letters[i] || *(s + count) == upp_letters[i])
+			{
+				*(s + count) = numbers[i];
+				break;
+			}
+		}
+		count++;
+	}
+
+	return (s);
 }
